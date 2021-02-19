6ea56f9967df38c415d3af95917273a567ccc653
Anele <Tnqari@gmail.com>
2021-02-19 14:21:18 +0200

    Joel

--- routes/hod.js
+++ routes/hod.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../config/config')
+//const https = require('https');
+//const fs = require('fs');
+const app = require('express')();
+const bodyParser = require('body-parser');
+//const cors = require('cors');
 
-/*var subjCode = '';
-var reportNo = '';
-var numStudents = '';
-var numAttended = '';
-var weekTreachings = '';
-var assess = '';
-var challenges = '';
-var departName = '';
-var hodName = '';
-var hodSurname = '';*/
+//const url = "C:/Users/ICEP-INTERN/Desktop/Anele/Weekly-Reports-back/Database/lectureReport.pdf";
 
+/*https.get(url, function(res){
+    const fileStream = fs.createWriteStream("C:/Users/ICEP-INTERN/Desktop/Anele/Weekly-Reports-back/Database/LecturerReport.pdf");
+    res.pipe(fileStream);
+    fileStream.on("finish", function(){
+        fileStream.close();
+    });
+});*/
 
 router.get('/hodreport', function (req, res, next) {
 
     const params = req.body
 
-    connection.query(`SELECT r.reportNum, r.numStudents, r.attendAvg, r.activities, r.assess, r.challRecomm, s.subjCode, d.deptName
-                        FROM reports r, lecture_subject ls, subject s, lecture l, department d, hod h
-                        WHERE ls.subjCode = s.subjCode 
-                        AND ls.lecNum = l.lecNum
-                        AND d.depCode = h.depCode
+    connection.query(`SELECT DISTINCT r.reportNum, r.activities, r.assess, r.challRecomm, r.date as start_date, date(r.date + 5) as end_date, s.subjCode, d.deptName 
+                        FROM reports r, lecture_subject ls, subject s, lecture l, department d, hod h 
+                        WHERE r.lecSubId = ls.lecSubId 
+                        AND ls.subjCode = s.subjCode 
+                        AND ls.lecNum = l.lecNum 
+                        AND d.depCode = h.depCode 
+                        AND week(date) = week(CURRENT_DATE) 
                         AND d.depCode = ?`, [params.depCode], function (error, results) {
 
         if (error) 
@@ -42,6 +47,8 @@ router.get('/hodreport', function (req, res, next) {
     
     });
 }); 
+
+
 
 //console.log('So far so good!')
 module.exports = router
